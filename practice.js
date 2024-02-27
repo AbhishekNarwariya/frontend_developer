d3b057686cd6e7685880f3c571081d15b5cab9a8
AbhishekNarwariya <narwariyaabhishek50@gmail.com>
2024-02-27 18:45:21 +0530

    adding form

---
+++ practice.js
@@ -0,0 +1,11 @@
+function myform(){
+
+    var name = document.getElementById('name').value
+    var password = document.getElementById('password').value
+    var age = document.getElementById('age').value
+
+    console.log(name);
+    console.log(password);
+    console.log(age);
+
+}
